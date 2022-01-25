238268183e2251d4b320788515ca3250819738da
Jose Posada <jdposa@gmail.com>
2022-01-24 19:31:46 -0500

    modifiying run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run -it --net host -v /home/jposada/Documents/github_repos:/home/workdir --entrypoint /bin/sh hugo_backend
+sudo docker run -it --net host -v /home/jposada/Documents/github_repos:/home/workdir --entrypoint /bin/sh jdposa/hugo_backend
